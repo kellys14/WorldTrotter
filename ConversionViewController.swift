c88a50fa77e3a2eba3672dc359f5c05386a7a08c
Sean Melnick Kelly <seanmelnickkelly@seans-macbook-pro.local>
2017-01-25 20:27:36 -0500

    Added new "ConversionViewController.swift" and removed "ViewController.swift"...still need to add code. Stopped at top of page 76

---
+++ ConversionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Sean Melnick Kelly on 1/25/17.
+//  Copyright © 2017 Sean Melnick Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+}
