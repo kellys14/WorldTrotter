7782667b9c77b8db4d43dfb26b04f06e27e00eb9
Sean Melnick Kelly <seanmelnickkelly@seans-macbook-pro.local>
2017-02-08 11:53:40 -0500

    Updated to chapter 5 with tabs

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Sean Melnick Kelly on 2/8/17.
+//  Copyright © 2017 Sean Melnick Kelly. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view")
+    }
+}
