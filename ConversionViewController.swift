84ca226bb396583ff11b4675417ac13d971a18bd
Sean Kelly <skelly@sub-mariner.highpoint.edu>
2017-02-13 13:45:13 -0500

    Restarted WebViewController. Use the same steps as MapView...

--- ConversionViewController.swift
+++ ConversionViewController.swift
@@ -47,10 +47,10 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
         
         print("ConversionViewController loaded it's view.")
         
-        let date = NSDate()
-        let calendar = NSCalendar.current
-        let hour = calendar.component(.hour, from: date as Date)
-        
+//        let date = NSDate()
+  //      let calendar = NSCalendar.current
+    //    let hour = calendar.component(.hour, from: date as Date)
+        let hour = 19
         let color = ["Morning": UIColor.blue, "Evening" : UIColor.darkGray]
         
         switch hour {
